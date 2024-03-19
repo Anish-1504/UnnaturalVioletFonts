7b7f38a53e673fba5efc942984daafbb74080e30
Anish Patil <123080347+Anish-1504@users.noreply.github.com>
2024-03-19 20:02:31 +0000

    practicing the code

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-num = int(input("Enter the value of number: "))
-if(num < 0):
-  print("Number is negative.")
-elif(num == 0):
-  print("number is zero.")
+num = int(input("Enter a number: "))
+if (num < 0):
+  print("no is neg")
+elif(num > 0):
+  if(num <= 10):
+    print("no is between 1-10")
+  elif(num > 10 and num <= 20):
+    print("no is between 11-20")
+  else:
+    print("no is greater than 20")
 else:
-  print("Number is positive.")
+  print("no is zero")
